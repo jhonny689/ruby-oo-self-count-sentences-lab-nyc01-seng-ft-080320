0e10f69341d2dcbd9f85b06462c97e93c540affc
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-04 21:09:18 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self =~ /[.!?]/ #is self match the regular expression
+      res = self.split(/[.!?]\s+/)
+      return res.length
+    else
+      return 0
+    end
   end
 end
